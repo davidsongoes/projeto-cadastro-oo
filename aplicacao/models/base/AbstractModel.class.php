4652b72bcc563bb1828efc3b1ba8250a039aa9fe
Davidson Góes <davidsongoes@gmail.com>
2017-06-13 11:02:48 -0300

    Refatorando metodo parseNameId, verificaDados

--- aplicacao/models/base/AbstractModel.class.php
+++ aplicacao/models/base/AbstractModel.class.php
@@ -1,65 +1,88 @@
 <?php
 
 namespace models\base;
+
 use components\PDOUtil;
+
 abstract class AbstractModel
 {
     public $id;
 
     public function eNovo()
     {
-        return !isset( $this->id ) || is_null( $this->id );
+        return !isset($this->id) || is_null($this->id);
     }
-    static public function verificaSecao(){
+
+    static public function verificaSecao()
+    {
         $logado = false;
-        if(isset($_SESSION['usuario_logado'])){
+        if (isset($_SESSION['usuario_logado'])) {
             $logado = true;
         }
         return $logado;
     }
+    public static function camposNulos()
+    {
+        $campoNulos = [
+            'saram',
+            'rtcaer'
+        ];
+        return $campoNulos;
+    }
     static public function verificaDados($dados){
         $verifica = true;
-        foreach($dados as $chave => $dado){
-            if(empty($dado)){
-                $verifica = false;
-                echo "o campo $chave esta vazio";
-                die();
+        foreach(self::camposNulos() as $key => $valor) {
+            foreach ($dados as $chave => $dado) {
+                if (empty($dado) AND $key != $dado AND $key == "") {
+                    $verifica = false;
+                    echo "o campo $chave esta vazio";
+                    die();
+                }
             }
         }
         return $verifica;
     }
-    static public function formataData($data){
+    static public function formataData($data)
+    {
         $arrayData = explode('/', $data);
         $data = $arrayData[2] . '-' . $arrayData[1] . '-' . $arrayData[0];
         return $data;
     }
-    static public function desformataData($data){
+
+    static public function desformataData($data)
+    {
         $arrayData = explode('-', $data);
-        $data = $arrayData[2].'/'.$arrayData[1].'/'.$arrayData[0];
+        $data = $arrayData[2] . '/' . $arrayData[1] . '/' . $arrayData[0];
         return $data;
     }
-    static public function executar($sql, $dados){
+
+    static public function executar($sql, $dados)
+    {
 
         $stmt = PDOUtil::getInstance()->obterConexao()->prepare($sql);
         $stmt->execute($dados);
     }
 
-    static public function queryAll($sql,$dados){
+    static public function queryAll($sql, $dados)
+    {
         $stmt = PDOUtil::getInstance()->obterConexao()->prepare($sql);
         $stmt->execute($dados);
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
-    static public function query($sql, $dados){
+
+    static public function query($sql, $dados)
+    {
         $stmt = PDOUtil::getInstance()->obterConexao()->prepare($sql);
         $stmt->execute($dados);
         return $stmt->fetch();
     }
-    static public function parseNameId($tabela, $coluna, $dado, $id){
+
+    static public function parseNameId($tabela, $coluna, $dado, $id)
+    {
         $query = "select $id from $tabela where $coluna = :$coluna";
         $stmt = PDOUtil::getInstance()->obterConexao()->prepare($query);
-        $stmt->execute(array(
-           $coluna => $dado
-        ));
-        return $stmt->fetch(\PDO::FETCH_ASSOC);
+        $stmt->execute(array($coluna => $dado));
+        $resultado = $stmt->fetch(\PDO::FETCH_ASSOC);
+        return (int)($resultado[$id]);
     }
 }
