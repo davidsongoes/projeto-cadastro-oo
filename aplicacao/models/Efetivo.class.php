bc3d5ced2f7462ce0216c4b2ec7fc9945d3125c9
Davidson Góes <davidsongoes@gmail.com>
2017-06-19 06:13:43 -0300

    Refatorando métodos

--- aplicacao/models/Efetivo.class.php
+++ aplicacao/models/Efetivo.class.php
@@ -16,22 +16,23 @@ class Efetivo extends EfetivoDAO
 
 //    MÉTODOS DO EFETIVO
 
-    public function novoMilitar($dados)
+    public static function adicionaMilitar($dados)
     {
         AbstractModel::verificaDados($dados);
         try {
             AbstractModel::executar(self::QUERY_INSERT, array(
-                ':id_posto_grad' => $dados['posto_grad'],
-                ':id_esp' => $dados['especialidade'],
-                ':id_quadro' => $dados['quadro'],
-                ':id_secao' => $dados['secao'],
                 ':saram' => $dados['saram'],
-                ':nome_completo' => $dados['nome_completo'],
-                ':nome_guerra' => $dados['nome_guerra'],
+                ':id_quadro' => $dados['quadro'],
+                ':id_posto_grad' => $dados['posto_graduacao'],
+                ':id_esp' => $dados['especialidade'],
                 ':situacao' => $dados['situacao'],
-                ':ramal' => $dados['ramal'],
+                ':nome_completo' => utf8_decode($dados['nome_completo']),
+                ':nome_guerra' => utf8_decode($dados['nome_guerra']),
                 ':data_nasc' => AbstractModel::formataData($dados['data_nascimento']),
-                ':data_ult_prom' => AbstractModel::formataData($dados['data_ult_promocao']),
+                ':data_ult_prom' => AbstractModel::formataData($dados['data_ultima_promocao']),
+                ':id_secao' => $dados['secao'],
+                ':ramal' => $dados['ramal'],
+                ':rtcaer' => $dados['rtcaer'],
                 ':email' => $dados['email'],
                 ':antiguidade_turma' => $dados['antiguidade_turma'],
             ));
@@ -48,14 +49,14 @@ class Efetivo extends EfetivoDAO
         try {
             AbstractModel::executar(self::QUERY_UPDATE, array(
                 ':saram' => $dados['saram'],
-                ':id_posto_grad' => AbstractModel::parseNameId('posto_grad', 'posto_grad', $dados['posto_graduacao'], 'id_posto_grad'),
-                ':id_quadro' => AbstractModel::parseNameId('quadro', 'quadro', $dados['quadro'], 'id_quadro'),
-                ':id_esp' => AbstractModel::parseNameId('especialidade', 'esp', $dados['especialidade'], 'id_esp'),
+                ':id_posto_grad' => $dados['posto_graduacao'],
+                ':id_quadro' => $dados['quadro'],
+                ':id_esp' => $dados['especialidade'],
                 ':nome_completo' => utf8_decode($dados['nome_completo']),
                 ':nome_guerra' => utf8_decode($dados['nome_guerra']),
                 ':data_nasc' => AbstractModel::formataData($dados['data_nascimento']),
                 ':data_ult_prom' => AbstractModel::formataData($dados['data_ultima_promocao']),
-                ':id_secao' => AbstractModel::parseNameId('secao', 'secao', $dados['secao'], 'id_secao'),
+                ':id_secao' => $dados['secao'],
                 ':ramal' => $dados['ramal'],
                 ':rtcaer' => $dados['rtcaer'],
                 ':situacao' => $dados['situacao'],
@@ -123,6 +124,7 @@ class Efetivo extends EfetivoDAO
         $resultado = AbstractModel::queryAll(self::QUERY_ALL_ESPECIALIDADE, null);
         return $resultado;
     }
+
     static public function allQuadro()
     {
         $resultado = AbstractModel::queryAll(self::QUERY_ALL_QUADRO, null);
@@ -161,20 +163,21 @@ class Efetivo extends EfetivoDAO
         return $efetivos;
     }
 
-    static public function buscarPorId($id){
+    static public function buscarPorId($id)
+    {
         $result = AbstractModel::query(self::QUERY_BY_ID, ARRAY(':id_militar' => $id));
-        if($result){
+        if ($result) {
             $efetivo = new Efetivo();
             $efetivo->id = $result['id_militar'];
             $efetivo->saram = $result['saram'];
             $efetivo->nome_completo = $result['nome_completo'];
             $efetivo->nome_guerra = $result['nome_guerra'];
-            $efetivo->posto_graduacao = Efetivo::buscaPostoGraduacao($result['id_posto_grad']);
-            $efetivo->especialidade = Efetivo::buscaEspecialidade($result['id_esp']);
-            $efetivo->quadro = Efetivo::buscaQuadro($result['id_quadro']);
+            $efetivo->posto_graduacao = $result['id_posto_grad'];
+            $efetivo->especialidade = $result['id_esp'];
+            $efetivo->quadro = $result['id_quadro'];
             $efetivo->situacao = $result['situacao'];
-            $efetivo->secao = Efetivo::buscaSecao($result['id_secao']);
-            $efetivo->ramal = ($result['ramal']);
+            $efetivo->secao = $result['id_secao'];
+            $efetivo->ramal = $result['ramal'];
             $efetivo->rtcaer = $result['rtcaer'];
             $efetivo->data_nascimento = $result['data_nasc'];
             $efetivo->data_ultima_promocao = $result['data_ult_prom'];
@@ -182,5 +185,18 @@ class Efetivo extends EfetivoDAO
             $efetivo->antiguidade_turma = $result['antiguidade_turma'];
         }
         return isset($efetivo) ? $efetivo : NULL;
+    }
+
+    public static function verificaFoto($posto_graduacao, $nome_guerra)
+    {
+        if (isset($posto_graduacao) AND ($nome_guerra)) {
+            $nome_arquivo = self::buscaPostoGraduacao($posto_graduacao) . '_' . utf8_encode($nome_guerra) . '.jpg';
+            if (file_exists("/var/www/html/fotos/$nome_arquivo")) {
+                return "../../../fotos/$nome_arquivo";
+            } else {
+                return '../../../fotos/sem_foto.jpg';
+            }
+        }
+
     }
 }
