d9de198e481832847283198f40f518fdc75eba9a
Davidson Góes <davidsongoes@gmail.com>
2017-06-19 06:14:43 -0300

    Alterando nome de campo da tabela

--- aplicacao/models/EfetivoDAO.class.php
+++ aplicacao/models/EfetivoDAO.class.php
@@ -11,7 +11,7 @@ use models\base\AbstractModel;
 
 class EfetivoDAO extends AbstractModel
 {
-    const QUERY_INSERT = "INSERT INTO militar (saram, nome_completo, id_posto_grad, id_esp, id_quadro, id_secao, nome_guerra, situacao, ramal, data_nasc, data_ult_prom, email, antiguidade_turma, ativo) VALUES ( :saram, :nome_completo, :id_post_grad, :id_esp, :id_quadro, :id_secao, :nome_guerra, :situacao, :ramal, :data_nasc, :data_ult_prom, :email, :antiguidade_turma, 1)";
+    const QUERY_INSERT = "INSERT INTO militar (saram, nome_completo, id_posto_grad, id_esp, id_quadro, id_secao, nome_guerra, situacao, ramal, rtcaer, data_nasc, data_ult_prom, email, antiguidade_turma, ativo) VALUES ( :saram, :nome_completo, :id_posto_grad, :id_esp, :id_quadro, :id_secao, :nome_guerra, :situacao, :ramal, :rtcaer, :data_nasc, :data_ult_prom, :email, :antiguidade_turma, 1)";
     const QUERY_UPDATE = "UPDATE militar SET id_posto_grad=:id_posto_grad, id_esp=:id_esp, id_quadro=:id_quadro, id_secao=:id_secao, saram=:saram, nome_completo=:nome_completo, nome_guerra=:nome_guerra, situacao=:situacao, ramal=:ramal, rtcaer=:rtcaer, data_nasc=:data_nasc, data_ult_prom=:data_ult_prom, email=:email, antiguidade_turma=:antiguidade_turma WHERE id_militar=:id_militar";
     const QUERY_DELETE = "UPDATE militar set ativo = 0 where id_militar = :id_militar";//A query não deleta o registro, somente desativa a visualização no lista de usuários
     const QUERY_BY_ID = "SELECT * FROM militar WHERE id_militar=:id_militar";
