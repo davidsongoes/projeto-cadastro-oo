93903eaba9a8629208db0303c5246e9388f97daa
Davidson Góes <davidsongoes@gmail.com>
2017-06-19 06:12:45 -0300

    Refatorando chamada de métodos

--- aplicacao/views/efetivo/detalhesMilitar.php
+++ aplicacao/views/efetivo/detalhesMilitar.php
@@ -2,7 +2,7 @@
 include(__DIR__ . '/../layout/header.php');
 use components\Helper;
 use models\base\AbstractModel;
-
+use models\Efetivo;
 ?>
     <div class="row">
         <div class="col-lg-2">
@@ -15,25 +15,25 @@ use models\base\AbstractModel;
                     <li class="breadcrumb-item"><a href="index.php?c=efetivo&acao=listarMilitares">Lista do Efetivo</a>
                     </li>
                     <li class="breadcrumb-item active">Detalhes
-                        - <?php echo $efetivo->posto_graduacao . ' ' . utf8_encode($efetivo->nome_guerra) ?></li>
+                        - <?php echo Efetivo::buscaPostoGraduacao($efetivo->posto_graduacao) . ' ' . utf8_encode($efetivo->nome_guerra) ?></li>
                 </ol>
                 <h1>Detalhes do Militar</h1>
                 <div class="row">
-                    <img src="../../../fotos/<?php echo $efetivo->posto_graduacao . '_' . utf8_encode($efetivo->nome_guerra) . '.jpg' ?>"
+                    <img src="<?php echo Efetivo::verificaFoto($efetivo->posto_graduacao, $efetivo->nome_guerra)?>"
                          style="margin-left: 15px" class="img-thumbnail" width="175" height="225">
                 </div>
                 <div class="row">
                     <div class="col-lg-6">
                         <h5>Saram: <?php echo $efetivo->saram ?></h5>
-                        <h5>Posto/Graduação: <?php echo $efetivo->posto_graduacao ?></h5>
-                        <h5>Quadro: <?php echo $efetivo->quadro ?></h5>
-                        <h5>Especialidade: <?php echo $efetivo->especialidade ?></h5>
-                        <h5>Seção: <?php echo $efetivo->secao ?></h5>
+                        <h5>Posto/Graduação: <?php echo Efetivo::buscaPostoGraduacao($efetivo->posto_graduacao) ?></h5>
+                        <h5>Quadro: <?php echo Efetivo::buscaQuadro($efetivo->quadro) ?></h5>
+                        <h5>Especialidade: <?php echo Efetivo::buscaEspecialidade($efetivo->especialidade) ?></h5>
+                        <h5>Seção: <?php echo Efetivo::buscaSecao($efetivo->secao) ?></h5>
                         <h5>Ramal: <?php echo $efetivo->ramal ?></h5>
                         <?php echo (!empty($efetivo->rtcaer)) ? "<h5>RTCAER: $efetivo->rtcaer </h5>" : ""; ?>
                     </div>
                     <div class="col-lg-6">
-                        <h5>Situação: <?php echo $efetivo->situacao ?></h5>
+                        <h5>Situação: <?php echo Helper::$situacao[$efetivo->situacao] ?></h5>
                         <h5>Nome Completo: <?php echo utf8_encode($efetivo->nome_completo) ?></h5>
                         <h5>Nome de Guerra: <?php echo utf8_encode($efetivo->nome_guerra) ?></h5>
                         <h5>Data de
