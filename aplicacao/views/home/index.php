2d48531d491e7cf26f9809d9dfdb7ba670e11478
Davidson Góes <davidsongoes@gmail.com>
2017-06-19 10:52:55 -0300

    Implementando breadcrumb

--- aplicacao/views/home/index.php
+++ aplicacao/views/home/index.php
@@ -2,10 +2,14 @@
         include(__DIR__ . '/../layout/header.php');
 ?>
 <div class="row">
-        <div class="col-lg-3">
+        <div class="col-lg-2">
                 <?php  include(__DIR__ . '/../layout/menu.php');?>
         </div>
-        <div class="col-lg-9">
+    <div class="container-fluid margem_direita">
+        <div class="col-lg-10">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            </ol>
                 <?php
                 \components\Helper::mostraAlerta("success");
                 \components\Helper::mostraAlerta("danger");
@@ -13,6 +17,7 @@
                 <p><b>Seja Bem-Vindo</b></p>
                 <p>Nosso Sistema de Controle de Efetivo agilizará seu processo</p>
         </div>
+    </div>
 </div>
 
 
