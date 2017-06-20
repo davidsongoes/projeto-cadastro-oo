7bed5516f25267fe73ae7766f93d7588bd94c204
Davidson Góes <davidsongoes@gmail.com>
2017-06-20 11:22:51 -0300

    Refatorando layout

--- aplicacao/views/usuario/detalhe.php
+++ aplicacao/views/usuario/detalhe.php
@@ -3,26 +3,31 @@ include(__DIR__ . '/../layout/header.php');
 use components\Helper;
 ?>
 <div class="row">
-    <div class="col-lg-3">
+    <div class="col-lg-2">
         <?php  include(__DIR__ . '/../layout/menu.php');?>
     </div>
-    <div class="col-lg-9">
-
+    <div class="container-fluid margem_direita">
+        <div class="col-lg-10">
+            <!--        Breadcrumbs-->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item"><a href="index.php?c=usuario&acao=exibir">Lista de Usuários</a>
+                <li class="breadcrumb-item active">Detalhes do Usuário - <?php echo $usuario->login?></li>
+            </ol>
 <h1>Detalhes do Usuario</h1>
-    <h2>Login:<?php echo " ".$usuario->login;?></h2>
+
 <div class="row">
     <div class="col-lg-6">
-        <h4>Posto/Graduação: <?php echo " ".Helper::$posto_graduacoes[$usuario->postoGraduacao] ?></h4>
-        <h4>Especialidade: <?php echo " ".Helper::$especialidades[$usuario->especialidade] ?></h4>
-        <h4>Seção: <?php echo " ".Helper::$secoes[$usuario->secao] ?></h4>
-        <h4>Grupo: <?php echo " ".Helper::$grupos[$usuario->grupo]; ?></h4>
+        <h5>Login:<?php echo " ".$usuario->login;?></h5>
+        <h5>Saram: <?php echo " ".$usuario->saram ?></h5>
+        <h5>Nome: <?php echo " ".$usuario->nome; ?></h5>
+        <h5>Email: <?php echo " ".$usuario->email; ?></h5>
+        <h5>Ativo: <?php echo " ". Helper::$ativo[$usuario->ativo] ?></h5>
+        <h5>Grupo: <?php echo " ".Helper::$grupos[$usuario->grupo]; ?></h5>
     </div>
     <div class="col-lg-6">
-        <h4>Saram: <?php echo " ".$usuario->saram ?></h4>
-        <h4>Nome: <?php echo " ".$usuario->nome; ?></h4>
-        <h4>Data de Nascimento: <?php echo " ".$usuario->dataNascimento ?></h4>
-        <h4>Data de Praça: <?php echo " ".$usuario->dataPraca ?></h4>
-        <h4>Data da Ultima Promoção: <?php echo " ".$usuario->dataUltimaPromocao ?></h4>
+
+
     </div>
 </div>
 </div>
