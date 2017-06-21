c359b9237b7a503964e3bba73b5ee78b62f62207
Davidson Góes <davidsongoes@gmail.com>
2017-06-21 16:45:16 -0300

    refatorando caminho de arquivos

--- aplicacao/views/layout/footer.php
+++ aplicacao/views/layout/footer.php
@@ -5,10 +5,10 @@
     <small>Copyright © <?=date('Y');?> | DIRENS</small>
     <br/>
     Email: <a href="mailto:santanarscs@fab.mil.br">3S SIN Santana</a> / <a href="mailto:goesdgbp@fab.mil.br">3S SIN Góes</a><br/>
-    <img src="../dist/images/face.jpeg" alt=""/>
-    <img src="../dist/images/twiter.jpg" alt=""/>
-    <img src="../dist/images/gmail.png" alt=""/>
-    <img src="../dist/images/github.png" alt=""/>
+    <img src="dist/images/face.jpeg" alt=""/>
+    <img src="dist/images/twiter.jpg" alt=""/>
+    <img src="dist/images/gmail.png" alt=""/>
+    <img src="dist/images/github.png" alt=""/>
 </footer>
 </div>
 </div>
