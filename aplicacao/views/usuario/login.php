df087de05d19ce4224f314b952bcceb97597f8b4
Davidson Góes <davidsongoes@gmail.com>
2017-06-21 11:40:31 -0300

    Refatorando modal cadastrar

--- aplicacao/views/usuario/login.php
+++ aplicacao/views/usuario/login.php
@@ -7,6 +7,7 @@
  */
 include(__DIR__ . '/../layout/header.php');
 use components\Helper;
+
 ?>
 <div class="container">
     <div class="login">
@@ -19,18 +20,23 @@ use components\Helper;
             <div class="panel-body">
                 <h3><b>SISCONE</b><br>Sistema de Controle de Efetivo</h3>
                 <hr/>
-                <form class="form-horizontal" role="form" method="post" action="index.php?c=usuario&acao=autenticarUsuario">
+                <form class="form-horizontal" role="form" method="post"
+                      action="index.php?c=usuario&acao=autenticarUsuario">
 
                     <div class="form-group">
-                        <label class="col-sm-1 col-sm-offset-3 control-label"><span class="glyphicon glyphicon-user"></span></label>
+                        <label class="col-sm-1 col-sm-offset-3 control-label"><span
+                                    class="glyphicon glyphicon-user"></span></label>
                         <div class="col-sm-5">
-                            <input type="text" placeholder="Usuário" class="form-control" name="login" required="required"/>
+                            <input type="text" placeholder="Usuário" class="form-control" name="login"
+                                   required="required"/>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="col-sm-1 col-sm-offset-3 control-label"><span class="glyphicon glyphicon-lock"></span></label>
+                        <label class="col-sm-1 col-sm-offset-3 control-label"><span
+                                    class="glyphicon glyphicon-lock"></span></label>
                         <div class="col-sm-5">
-                            <input type="password" placeholder="Senha" class="form-control" name="senha" required="required"/>
+                            <input type="password" placeholder="Senha" class="form-control" name="senha"
+                                   required="required"/>
                         </div>
                     </div>
                     <input type="submit" class="btn btn-primary" value="Entrar"/>
@@ -43,7 +49,7 @@ use components\Helper;
 
         </div>
     </div>
-    </div>
+</div>
 
 
 <!--INICIO DA MODAL SENHA-->
@@ -51,21 +57,27 @@ use components\Helper;
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title">Recuperar Senha</h4>
             </div>
             <div class="modal-body">
-                <form  class="form-horizontal" role="form" method="post" action="index.php?c=usuario&acao=recuperarSenha">
+                <form class="form-horizontal margem_direita" role="form" method="post"
+                      action="index.php?c=usuario&acao=recuperarSenha">
                     <div class="form-group">
                         <label for="email" class="col-sm-3 control-label">Email</label>
                         <div class="col-sm-9">
-                            <input type="email" class="form-control" id="email" placeholder="Email cadastrado" name="email" required="required">
+                            <input type="email" class="form-control" id="email" placeholder="Email cadastrado"
+                                   name="email" required="required">
                         </div>
                     </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Fechar</button>
-                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-thumbs-up"></span> Enviar</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span
+                            class="glyphicon glyphicon-remove"></span> Fechar
+                </button>
+                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-thumbs-up"></span> Enviar
+                </button>
             </div>
             </form>
         </div><!-- /.modal-content -->
@@ -77,59 +89,61 @@ use components\Helper;
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Cadastro</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Recuperar Senha</h4>
             </div>
             <div class="modal-body">
-                <div class="container-fluid">
-                <form class="form-horizontal" role="form" method="post" action="index.php?c=usuario&acao=novo">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="form-group">
-                                <label for="saram" class="col-sm-3 control-label">SARAM</label>
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" id="saram" placeholder="SARAM"
-                                           name="saram" required="required" value="<?= $usuario->saram ?>">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="login" class="col-sm-3 control-label">Login</label>
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" id="login" placeholder="Nome de guerra e iniciais" name="login"
-                                           required="required" value="<?php echo $usuario->login ?>">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="nome" class="col-sm-3 control-label">Nome Completo</label>
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" id="nome" placeholder="Nome Completo" name="nome"
-                                           required="required" value="<?php echo $usuario->nome ?>">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="email" class="col-sm-3 control-label">E-mail Intraer</label>
-                                <div class="col-sm-9">
-                                    <input type="email" class="form-control" id="email"
-                                           placeholder="exemplo@fab.mil.br" name="email" required="required"
-                                           value="<?php echo $usuario->email ?>">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="senha" class="col-sm-3 control-label">Senha</label>
-                                <div class="col-sm-9">
-                                    <input type="password" class="form-control" id="senha" placeholder="Senha"
-                                           name="senha" required="required" value="<?php echo $usuario->senha ?>">
-                                </div>
-                            </div>
+                <form class="form-horizontal margem_direita" role="form" method="post"
+                      action="index.php?c=usuario&acao=novoCadastro">
+                    <div class="form-group">
+                        <label for="saram" class="col-sm-3 control-label">SARAM</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="saram" placeholder="SARAM"
+                                   name="saram" required="required" value="<?= $usuario->saram ?>">
                         </div>
-                </form>
-                </div>
-        </div><!-- /.modal-content -->
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Fechar</button>
-                <button type="submit" class="btn btn-default"><span
-                            class="glyphicon glyphicon-thumbs-up"></span> Enviar</button>
+                    </div>
+                    <div class="form-group">
+                        <label for="login" class="col-sm-3 control-label">Login</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="login"
+                                   placeholder="Nome de guerra e iniciais" name="login"
+                                   required="required" value="<?php echo $usuario->login ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="nome" class="col-sm-3 control-label">Nome Completo</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="nome" placeholder="Nome Completo"
+                                   name="nome"
+                                   required="required" value="<?php echo $usuario->nome ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="col-sm-3 control-label">E-mail Intraer</label>
+                        <div class="col-sm-9">
+                            <input type="email" class="form-control" id="email"
+                                   placeholder="exemplo@fab.mil.br" name="email" required="required"
+                                   value="<?php echo $usuario->email ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="senha" class="col-sm-3 control-label">Senha</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" id="senha" placeholder="Senha"
+                                   name="senha" required="required" value="<?php echo $usuario->senha ?>">
+                        </div>
+                    </div>
             </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span
+                            class="glyphicon glyphicon-remove"></span> Fechar
+                </button>
+                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-thumbs-up"></span> Enviar
+                </button>
+            </div>
+            </form>
+        </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
